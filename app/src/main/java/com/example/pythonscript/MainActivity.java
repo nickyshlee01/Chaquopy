fc53f42e23656a067203e8c45e454988d9d20462
nickyshlee01 <nickyshlee01@gmail.com>
2021-06-01 22:49:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pythonscript/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.pythonscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (! Python.isStarted()) {
+                    Python.start(new AndroidPlatform(getApplicationContext()));
+                }
+
+                Python python = Python.getInstance();
+                PyObject practice = python.getModule("practice");
+                int sum = practice.callAttr("test").toInt();
+
+                text.setText(String.valueOf(sum));
+            }
+        });
+    }
+}
---
+++ app/src/main/python/practice.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def test():
+
+    arr = np.array([1, 2, 3])
+    sum = arr.sum()
+    
+    return sum
+
+
+
+
+
